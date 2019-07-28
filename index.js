b76dbea53803fe5182bf7c5b8a712d385fd761aa
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-07-28 11:58:25 -0500

    eventlistener added to projects link and img folder updated

---
+++ index.js
@@ -0,0 +1,13 @@
+// window.onload = function(){
+//     document.getElementById("proj-link").onclick = function() {
+//     document.getElementById("sidebar").innerHTML = "Projects!";
+//     document.getElementById("sidebar").style = "color: #5a037b";
+// }
+// }
+
+window.onload = function() {
+    document.getElementById("proj-link").onclick = function(){
+        document.getElementById("sidebar").style.color = "#5a037b";
+        document.getElementById("sidebar").innerHTML = "Projects";
+}
+}
